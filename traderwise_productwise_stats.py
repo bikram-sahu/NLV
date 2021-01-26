c940a1fbced7da4bf2bd4fdccd0cae12a346fbf3
Bikram Keshari Sahu <52929215+bikram-sahu@users.noreply.github.com>
2021-01-26 17:22:40 +0530

    Removed Signup

--- traderwise_productwise_stats.py
+++ traderwise_productwise_stats.py
@@ -40,9 +40,9 @@ def view_all_users():
 
 
 def main():
-    st.sidebar.title("Month to Date Productwise Statistics")
+    st.title("Month to Date Productwise Statistics")
     app_mode = st.sidebar.selectbox("Mode",
-                                    ["Login", "SignUp"])
+                                    ["Login"])
 
     if app_mode == "Login":
         #st.subheader("Login Section")
@@ -60,18 +60,6 @@ def main():
                 run_analytics(analytics_by)
             else:
                 st.warning("Incorrect Username/Password")
-    
-    elif app_mode == "SignUp":
-        st.subheader("Create New Account")
-        new_user = st.text_input("Username")
-        new_password = st.text_input("Password",type='password')
-        
-        if st.button("Signup"):
-            create_usertable()
-            add_userdata(new_user,make_hashes(new_password))
-            st.success("You have successfully created a valid Account")
-            st.info("Go to Login Menu to login")
-    
 
 def upload_data():
     global qtr_data, volume_data

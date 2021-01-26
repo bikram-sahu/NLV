ff6dd4fb6d2c589834b5133d03c160a9fbb92b08
Bikram Keshari Sahu <52929215+bikram-sahu@users.noreply.github.com>
2021-01-26 17:09:56 +0530

    Added Authentication

--- traderwise_productwise_stats.py
+++ traderwise_productwise_stats.py
@@ -4,16 +4,73 @@ import pandas as pd
 import os
 import copy
 
+# Security
+#passlib,hashlib,bcrypt,scrypt
+import hashlib
+def make_hashes(password):
+	return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+	if make_hashes(password) == hashed_text:
+		return hashed_text
+	return False
+# DB Management
+import sqlite3 
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+# DB  Functions
+def create_usertable():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+
+
+def add_userdata(username,password):
+	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+	conn.commit()
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
+
+
+def view_all_users():
+	c.execute('SELECT * FROM userstable')
+	data = c.fetchall()
+	return data
+
+
 def main():
     st.sidebar.title("Month to Date Productwise Statistics")
     app_mode = st.sidebar.selectbox("Mode",
-                                    ["Analytics"])
+                                    ["Login", "SignUp"])
 
-    if app_mode == "Analytics":
-        #upload_data()
-        load_data()
-        analytics_by = st.sidebar.selectbox("Select", ["by Client", "Overall Stats"])
-        run_analytics(analytics_by)
+    if app_mode == "Login":
+        #st.subheader("Login Section")
+        username = st.sidebar.text_input("User Name")
+        password = st.sidebar.text_input("Password", type='password')
+        if st.sidebar.checkbox("Login"):
+            create_usertable()
+            hashed_pswd = make_hashes(password)
+            result = login_user(username,check_hashes(password,hashed_pswd))
+            if result:
+                st.sidebar.success("Logged In as {}".format(username))
+                #upload_data()
+                load_data()
+                analytics_by = st.sidebar.selectbox("Select", ["by Client", "Overall Stats"])
+                run_analytics(analytics_by)
+            else:
+                st.warning("Incorrect Username/Password")
+    
+    elif app_mode == "SignUp":
+        st.subheader("Create New Account")
+        new_user = st.text_input("Username")
+        new_password = st.text_input("Password",type='password')
+        
+        if st.button("Signup"):
+            create_usertable()
+            add_userdata(new_user,make_hashes(new_password))
+            st.success("You have successfully created a valid Account")
+            st.info("Go to Login Menu to login")
     
 
 def upload_data():

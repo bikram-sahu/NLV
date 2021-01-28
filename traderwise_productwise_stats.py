25eef41906f47da82b497b16d636dd0586f42951
Bikram Keshari Sahu <52929215+bikram-sahu@users.noreply.github.com>
2021-01-28 20:09:02 +0530

    Added Signup and Made changed to mumbai section.

--- traderwise_productwise_stats.py
+++ traderwise_productwise_stats.py
@@ -4,8 +4,7 @@ import pandas as pd
 import os
 import copy
 
-from st_aggrid import AgGrid
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
+
 
 import dropbox
 
@@ -51,10 +50,9 @@ def view_all_users():
 def main():
     st.sidebar.title("Month to Date Productwise Statistics")
     app_mode = st.sidebar.selectbox("Mode",
-                                    ["Login"])
+                                    ["Login", "SignUp"])
 
     if app_mode == "Login":
-        #st.subheader("Login Section")
         username = st.sidebar.text_input("User Name")
         password = st.sidebar.text_input("Password", type='password')
         if st.sidebar.checkbox("Login"):
@@ -63,20 +61,30 @@ def main():
             result = login_user(username,check_hashes(password,hashed_pswd))
             if result:
                 st.sidebar.success("Logged In as {}".format(username))
-                #upload_data()
                 productwise_data = "/MTDPL.xlsx"
                 transaction_data_file = "/Transactions Jan-21.xlsx"
-                MIS_file_name = "/MIS Mumbai.xlsx"
-                MIS_data = load_MIS_data(MIS_file_name)
                 qtr_data, transaction_data = load_data(productwise_data, transaction_data_file)
-                analytics_by = st.sidebar.selectbox("Select", ["by Client", "Overall Stats", "MIS"])
-                run_analytics(analytics_by, qtr_data, transaction_data, MIS_data)
+                transaction_raw, instrument_name = load_transaction_data(transaction_data_file)
+
+                analytics_by = st.sidebar.selectbox("Select", ["Mumbai", "Overall Stats"])
+                run_analytics(analytics_by, qtr_data, transaction_data, transaction_raw, instrument_name)
             else:
                 st.warning("Incorrect Username/Password")
+                
+    elif app_mode == "SignUp":
+        st.subheader("Create New Account")
+        new_user = st.text_input("Username")
+        new_password = st.text_input("Password",type='password')
+        
+        if st.button("Signup"):
+            create_usertable()
+            add_userdata(new_user,make_hashes(new_password))
+            st.success("You have successfully created a valid Account")
+            st.info("Go to Login Menu to login")
+    
 
 @st.cache
 def load_data(productwise_data, transaction_data_file):
-    #st.date_input('Date input (currently this is not linked)')
     global qtr_data, transaction_data
 
     _, f = dbx.files_download(productwise_data)
@@ -85,151 +93,90 @@ def load_data(productwise_data, transaction_data_file):
     qtr_data = pd.read_excel(f, sheet_name = "Gross PNL By Product", engine='openpyxl')
     qtr_data = qtr_data[qtr_data['Client'].map(len) <= 5]
     qtr_data.drop(columns = ["Contract Type", "Base Currency", "AUD", "CAD", "CHF", "EUR", "GBP", "HKD", "JPY", "NOK", "SEK", "USD"], inplace=True)
-    #if st.checkbox('Show data'):
-    #    st.write(qtr_data)
     _, f = dbx.files_download(transaction_data_file)
     f = f.content
     transaction_data = pd.read_excel(f, sheet_name="Sheet2", engine='openpyxl', skiprows = 1)
     transaction_data.drop([len(transaction_data)-1], inplace=True)
     transaction_data.drop(columns = ["Grand Total"], inplace=True)
     transaction_data.set_index("Row Labels", inplace=True)
-    #st.write(transaction_data)
     return qtr_data, transaction_data
 
-@st.cache
-def load_MIS_data(MIS_file_name):
-    global MIS_data
+@st.cache(allow_output_mutation=True)
+def load_transaction_data(transaction_data_file):
+    global transaction_raw
 
-    _, f = dbx.files_download(MIS_file_name)
+    _, f = dbx.files_download(transaction_data_file)
     f = f.content
+    transaction_raw = pd.read_excel(f, sheet_name="Sheet3", engine='openpyxl')
 
-    MIS_data = pd.read_excel(f, sheet_name = "Stats", engine='openpyxl', skiprows=1, nrows= 54)
-    MIS_data.drop(columns = ["Unnamed: 4", "Unnamed: 8", "Unnamed: 12"], inplace= True)
-    return MIS_data
+    instrument_name = pd.read_excel(f, sheet_name="Sheet5", engine='openpyxl')
+    transaction_raw.drop([0, 85, 86], inplace=True)
+    return transaction_raw, instrument_name
 
-@st.cache
+
+
 def color_negative_red(val):
     """
     Takes a scalar and returns a string with
     the css property `'color: red'` for negative
     strings, black otherwise.
     """
-    color = 'red' if val < 0 else 'black'
+    color = 'red' if val < 0 else 'green'
     return 'color: %s' % color
 
-    
 
-def run_analytics(analytics_by, qtr_data, transaction_data, MIS_data):
-    if analytics_by == "by Client":
-        
+def run_analytics(analytics_by, qtr_data, transaction_data, transaction_raw, instrument_name):
+    if analytics_by == "Mumbai":
         by_client = qtr_data.groupby(['Client'], as_index=False)
-        #st.write(type(by_client))
-        #st.write(qtr_data.groupby(['Client'], as_index=False).sum())
-        #st.bar_chart(by_client.sum()["Total"])
+        
 
         with open("mumbai_traders.txt") as file:
             mumbai_traders_list = [line.strip() for line in file]
-        #by_client[by_client["Client"] =]
+
         
         st.markdown("**Traderwise Stats (Mumbai)**")
-        #client_num = st.selectbox("Select a Client Id", qtr_data['Client'].unique())
         client_num = st.selectbox("Select a Client Id", mumbai_traders_list)
         per_client = by_client.get_group(client_num)
-        
-        #col1, col2 = st.beta_columns((2,1))
-        #col1.write(per_client.groupby(["Contract Group", "Contract Sub Group", "Contract Code"], as_index=False).sum())
-        #col2.write(transaction_data.loc[client_num].dropna().div(2))
-        #st.write(type(per_client.groupby(["Contract Group", "Contract Sub Group", "Contract Code"]).sum()))
-        #st.write(type(transaction_data.loc[client_num].dropna().div(2)))
+
         df1 = per_client.groupby(["Contract Group", "Contract Sub Group", "Contract Code"], as_index=False).sum()
         df2 = transaction_data.loc[client_num].fillna(0).div(2)
-
         df1["RT"] = df1["Contract Code"].apply(lambda x: df2.loc[x])
-        df = df1.round(1)
-        ########LAST EDIT##########
-        #st.write(df1)
-        gb = GridOptionsBuilder.from_dataframe(df)
-        cellsytle_jscode = JsCode("""
-        function(params) {
-            if (params.value < 0) {
-                return {
-                    'color': 'white',
-                    'backgroundColor': 'red'
-                }
-            } else {
-                return {
-                    'color': 'white',
-                    'backgroundColor': 'green'
-                }
-            }
-        };
-        """)
-        gb.configure_column("Total", cellStyle=cellsytle_jscode)
-        gb.configure_grid_options(domLayout='normal')
-        gridOptions = gb.build()
 
-        #Display the grid
-        with st.spinner("Loading Grid..."):
-            #st.header("Streamlit Ag-Grid")
-            grid_response = AgGrid(
-                df, 
-                gridOptions=gridOptions, 
-                width='100%',
-                allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
-                )
+        mumbai = pd.DataFrame()
+        for trader in mumbai_traders_list:
+            df = by_client.get_group(trader)
+            df11 = df.groupby(["Contract Code"], as_index=False).sum()
+            df22 = transaction_data.loc[trader].fillna(0).div(2)
+            df11["RT"] = df11["Contract Code"].apply(lambda x: df22.loc[x])
+            result = pd.concat([mumbai, df11])
+            mumbai = result
+        contract_names = copy.deepcopy(transaction_raw)
+        contract_names.drop(columns=["Sum of Qty"], inplace=True)
+        contract_names = contract_names.rename(columns={"Instruments": "Contract Code"}, errors="raise") 
+        df1 = df1.style.applymap(color_negative_red, subset=pd.IndexSlice[:, ['Total']])
+        st.dataframe(df1.format({'RT': '{:.0f}', 'Total': '{:.0f}'}), height=2500)
 
-        df = grid_response['data']
-        selected = grid_response['selected_rows']
-        selected_df = pd.DataFrame(selected)
+        st.markdown("**All Traders (Mumbai)**")
+        mumbai = mumbai.groupby("Contract Code", as_index=False).sum()
+        mumbai_final = pd.merge(contract_names,  
+                      mumbai,  
+                      on ='Contract Code')
+        mumbai_final = mumbai_final.style.applymap(color_negative_red, subset=pd.IndexSlice[:, ['Total']])
+        st.dataframe(mumbai_final.format({"RT": '{:.0f}', 'Total': '{:.0f}'}), height=2500)
         
         
 
     elif analytics_by == "Overall Stats":
-        st.write(qtr_data.groupby(['Contract Group']).sum())
-        by_contract = qtr_data.groupby(['Contract Group']).sum() 
-        st.bar_chart(by_contract["Total"])
-
-    elif analytics_by == "MIS":
-        #MIS_data = MIS_data.style.applymap(color_negative_red)
-        df = MIS_data.round(1)
-        gb = GridOptionsBuilder.from_dataframe(df)
-        cellsytle_jscode = JsCode("""
-        function(params) {
-            if (params.value <= 0.5) {
-                return {
-                    'color': 'white',
-                    'backgroundColor': 'red'
-                }
-            } else {
-                return {
-                    'color': 'black',
-                    'backgroundColor': 'white'
-                }
-            }
-        };
-        """)
-        gb.configure_column("% of Up days", cellStyle=cellsytle_jscode)
-        gb.configure_grid_options(domLayout='normal')
-        gridOptions = gb.build()
-
-        #Display the grid
-        with st.spinner("Loading Grid..."):
-            st.header("Streamlit Ag-Grid")
-            grid_response = AgGrid(
-                df, 
-                gridOptions=gridOptions, 
-                width='100%',
-                allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
-                )
-
-        df = grid_response['data']
-        selected = grid_response['selected_rows']
-        selected_df = pd.DataFrame(selected)
-        #AgGrid(df)
-
+        
+        df = qtr_data.groupby(['Contract Group']).sum()
+        by_contract_code = qtr_data.groupby(['Contract Code'], as_index=False).sum()
+        by_contract_code.rename(columns = {"Contract Code": "Instruments"}, inplace=True)
+        inner_join = pd.merge(transaction_raw,  
+                      by_contract_code,  
+                      on ='Instruments')
+        inner_join = inner_join.style.applymap(color_negative_red, subset=pd.IndexSlice[:, ['Total']])
+        st.dataframe(inner_join.format({'Sum of Qty': '{:.0f}', 'Total': '{:.0f}'}), height=2500)
     
-
-
 
 if __name__ == "__main__":
     main()

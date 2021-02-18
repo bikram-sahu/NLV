f5a32d8fccba40b4c2ecaf33feaa59b0068c0cfb
Bikram Keshari Sahu <52929215+bikram-sahu@users.noreply.github.com>
2021-02-18 15:44:01 +0530

    Updated upto 17 Feb 2021

--- traderwise_productwise_stats.py
+++ traderwise_productwise_stats.py
@@ -86,7 +86,7 @@ def main():
             hashed_pswd = make_hashes(password)
             result = login_user(username,check_hashes(password,hashed_pswd))
             if result:
-                st.markdown("### Last updated data is as of: 16-Feb-2021")
+                st.markdown("### Last updated data is as of: 17-Feb-2021")
                 month_selected = st.sidebar.selectbox('Select a month', ["Feb 2021", "Jan 2021"])
                 #st.sidebar.success("Logged In as {}".format(username))
                 mtd_gross_pnl = "/" + month_selected + "/MTDPL.xlsx"
